c66b0913f888986db637af336fc253e2f569ea12
Beatris <beatrisraj7@gmail.com>
2025-06-04 19:26:12 +0300

    chore: add boilerplate combo checking

--- src/objects/ComboManager.ts
+++ src/objects/ComboManager.ts
@@ -2,12 +2,12 @@ import type { Direction } from "../types/directionType";
 export class ComboManager {
     combo: [number, Direction][];
     currentStep: number;
-    input: [number, Direction][];
+    accumulatedSteps: number;
 
     constructor() {
         this.combo = []
-        this.currentStep = 0
-        this.input = []
+        this.currentStep = 0;
+        this.accumulatedSteps = 0;
         this.generateCombo()
     }
 
@@ -16,23 +16,42 @@ export class ComboManager {
             Math.floor(Math.random() * 9) + 1,
             Math.random() > 0.5 ? 'clockwise' : 'counterclockwise',
         ]);
-        this.input = [];
         this.currentStep = 0;
+        this.accumulatedSteps = 0;
         console.log('Secret combination:', this.combo.map(([n, d]) => `${n} ${d}`).join(', '));
     }
 
     checkCombo(steps: number, dir: Direction) {
+        if (this.currentStep >= this.combo.length) return 'success';
+
         const [expectedSteps, expectedDir] = this.combo[this.currentStep];
 
-        if (steps === expectedSteps && dir === expectedDir) {
-            this.currentStep++;
-            if (this.currentStep === this.combo.length) {
-                console.log('Unlock Vault');
-
-            }
-        } else {
-            console.log('Reset Vault');
+        if (dir !== expectedDir) {
+            console.log('fail');
+            return 
         }
+
+        this.accumulatedSteps += steps;
+
+        if (this.accumulatedSteps > expectedSteps) {
+            console.log('fail');
+            return 
+        }
+
+        if (this.accumulatedSteps < expectedSteps) {
+            console.log('continue');
+            return 
+        }
+
+        this.currentStep++;
+        this.accumulatedSteps = 0;
+
+        if (this.currentStep === this.combo.length) {
+            console.log('✅ Vault Unlocked!');
+            return 'success';
+        }
+
+        return 'continue';
     }
 
 }
